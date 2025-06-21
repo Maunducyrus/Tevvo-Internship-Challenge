de87ef41d819a3320807ce654428034088e7d6c3
Maunducyrus <maunducyrus123@gmail.com>
2025-06-21 15:57:38 +0300

    npm install vue-router@4

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
-<script setup>
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <nav class="bg-white shadow mb-8">
+      <div class="container mx-auto flex justify-between items-center py-4">
+        <h1 class="text-2xl font-bold text-indigo-600">Tevvo Build Challenge</h1>
+        <div class="flex gap-4">
+          <router-link class="hover:underline" to="/">Home</router-link>
+          <router-link class="hover:underline" to="/countries">Countries</router-link>
+          <router-link class="hover:underline" to="/pokemon">Pokemon</router-link>
+        </div>
+      </div>
+    </nav>
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
