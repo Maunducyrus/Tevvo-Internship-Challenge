346bcc4b477b9d7c16014f26f8e615d708add2b9
Maunducyrus <maunducyrus123@gmail.com>
2025-06-21 16:00:09 +0300

    added countries content

---
+++ src/views/Countries.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto px-4">
+    <h2 class="text-xl font-bold mb-4">Welcome to the Tevvo Build Challenge!</h2>
+    <p>Explore country and Pokémon data using public APIs.</p>
+    <ul class="list-disc ml-6 mt-4">
+      <li>Browse <router-link class="text-indigo-600 underline" to="/countries">Countries</router-link></li>
+      <li>Browse <router-link class="text-indigo-600 underline" to="/pokemon">Pokémon</router-link></li>
+    </ul>
+  </div>
+</template>
