3ee2809c653be284c09597b2dd4a03ba9fa84815
Maunducyrus <maunducyrus123@gmail.com>
2025-06-21 15:19:40 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
