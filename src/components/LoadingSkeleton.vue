3268fbc7c326cfd74648fe4a551592350ddba964
Maunducyrus <maunducyrus123@gmail.com>
2025-06-23 06:52:45 +0300

    created a new file Loading Skeleton

---
+++ src/components/LoadingSkeleton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="animate-pulse flex flex-wrap gap-4">
+    <div
+      v-for="n in count"
+      :key="n"
+      class="bg-gray-200 dark:bg-gray-700 rounded h-24 w-48"
+    ></div>
+  </div>
+</template>
+<script setup>
+defineProps({ count: { type: Number, default: 4 } })
+</script>
