1d22e34fb451c269445e7e9a87fbd17cd7d6550e
Maunducyrus <maunducyrus123@gmail.com>
2025-06-23 06:35:56 +0300

    Created Favorites file and added some content

---
+++ src/views/Favorites.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container mx-auto px-4">
+    <FavoritesList />
+  </div>
+</template>
+<script setup>
+import FavoritesList from '../components/FavoritesList.vue'
+</script>
